6cd4982d2629a354aa23bc617885ecf381c98f6e
hostage112 <ess.aleksandr@gmail.com>
2017-04-26 23:09:27 +0300

    init

---
+++ BricsCommands/BricsCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+//ODA
+using Teigha.Runtime;
+using Teigha.DatabaseServices;
+using Teigha.Geometry;
+
+//Bricsys
+using Bricscad.ApplicationServices;
+using Bricscad.Runtime;
+using Bricscad.EditorInput;
+
+[assembly: CommandClass(typeof(commands.CadCommands))]
+namespace commands
+{
+    public class CadCommands
+    {
+        [CommandMethod("te")]
+        public void testing()
+        {
+
+        }
+    }
+}
---
+++ CadCommands/CadCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections;
+using System.Linq;
+using System.IO;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+[assembly: CommandClass(typeof(commands.CadCommands))]
+namespace commands
+{
+    public class CadCommands
+    {
+        [CommandMethod("te")]
+        public void testing()
+        {
+            
+        }
+    }
+}
