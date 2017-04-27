38d5fcfeb8b0d8f8e00f9e806a67ed3995c96488
hostage112 <ess.aleksandr@gmail.com>
2017-04-27 18:24:56 +0300

    working version

--- CadCommands/CadCommands.cs
+++ CadCommands/CadCommands.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Text;
-using System.Collections;
-using System.Linq;
-using System.IO;
-using System.Diagnostics;
-using System.Collections.Generic;
+﻿//using System;
+//using System.Text;
+//using System.Collections;
+//using System.Linq;
+//using System.IO;
+//using System.Diagnostics;
+//using System.Collections.Generic;
 
-using Autodesk.AutoCAD.Runtime;
-using Autodesk.AutoCAD.ApplicationServices;
-using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.Geometry;
-using Autodesk.AutoCAD.EditorInput;
+//using Autodesk.AutoCAD.Runtime;
+//using Autodesk.AutoCAD.ApplicationServices;
+//using Autodesk.AutoCAD.DatabaseServices;
+//using Autodesk.AutoCAD.Geometry;
+//using Autodesk.AutoCAD.EditorInput;
 
-[assembly: CommandClass(typeof(commands.CadCommands))]
-namespace commands
-{
-    public class CadCommands
-    {
-        [CommandMethod("te")]
-        public void testing()
-        {
+//[assembly: CommandClass(typeof(commands.CadCommands))]
+//namespace commands
+//{
+//    public class CadCommands
+//    {
+//        [CommandMethod("te")]
+//        public void testing()
+//        {
             
-        }
-    }
-}
+//        }
+//    }
+//}
